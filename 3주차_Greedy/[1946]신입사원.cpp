b145ef78de44c8113e40424b892c8ee9b37042e3
김원진 <sobaek2000@naver.com>
2022-03-29 15:48:55 +0900

    [BOJ]신입사원/ 실버1
    https://www.acmicpc.net/problem/1946

--- 3주차_Greedy/[1946]신입사원.cpp
+++ 3주차_Greedy/[1946]신입사원.cpp
@@ -30,15 +30,16 @@ int main()
 	{
 		cin >> N;
 		struct applicant a[N];
-		result = 1;
 
 		for (i = 0; i < N; i++)
 		{
 			cin >> a[i].document >> a[i].interview;
 		}
-		sort(a, a + N, cmp);
+		sort(a, a + N, cmp);     // 서류 성적 등수 순으로 정렬 (1등부터)
 		min = a[0].interview;
+		result = 1;              // 서류 1등은 일단 합격
 
+		// 나보다 서류 등수 높은 사람 중에 면접 등수 젤 높으면 합격
 		for (i = 1; i < N; i++)
 		{
 			if (min > a[i].interview)
