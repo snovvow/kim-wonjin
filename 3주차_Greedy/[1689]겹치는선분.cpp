01e543923f195edd8af717e2b9108b2afff40064
김원진 <sobaek2000@naver.com>
2022-04-01 01:47:52 +0900

    [BOJ]겹치는선분/ 골드4
    https://www.acmicpc.net/problem/1689

--- 3주차_Greedy/[1689]겹치는선분.cpp
+++ 3주차_Greedy/[1689]겹치는선분.cpp
@@ -1,23 +1,50 @@
 /* 겹치는 선분 */
 
+// 스위핑 알고리즘  -> 빗자루 처럼 쓸면서 훑기
+
  #include <iostream>
- #include <cstring>
- #include <vector>
  #include <algorithm>
 
 using namespace std;
 
+typedef struct point
+{
+	int		pos;
+	int		status;    // 시작점 : 1   끝점 : -1
+} point;
+
+bool cmp(point a, point b)
+{
+	if (a.pos != b.pos)
+		return (a.pos < b.pos);
+	return (a.status < b.status);   // 끝점이 겹칠 경우 포함 시키지 않기 위해
+}
+
 int main()
 {
-	int N, i, tmp1, tmp2;
+	int N, i, line, max;
 
 	cin >> N;
-	vector<pair<int, int> > line;
-	for (i = 0; i < N; i++)
+	point point[2 * N];   // 모든 시작점 끝점 저장
+
+	for (i = 0; i < 2 * N; i += 2)
 	{
-		cin >> tmp1 >> tmp2;
-		line.push_back(make_pair(tmp1, tmp2));
+		cin >> point[i].pos;
+		point[i].status = 1;
+
+		cin >> point[i + 1].pos;
+		point[i + 1].status = -1;
 	}
 
-	cout <<  
+	sort(point, point + 2 * N, cmp);
+	line = 0;
+	max = 0;
+
+	for (i = 0; i < 2 * N; i++)
+	{
+		line += point[i].status;   //  ++ or --
+		if (max < line)
+			max = line;
+	}
+	cout << max;
 }
